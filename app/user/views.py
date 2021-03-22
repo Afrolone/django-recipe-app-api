49782badaa7083a4d4a54c95be2ad80fa9f15397
Afrolone <muratow_nur@hotmail.com>
2021-03-22 21:27:14 +0100

    Added delete request to User endpoint.

--- app/user/views.py
+++ app/user/views.py
@@ -1,8 +1,11 @@
 from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from user.serializers import UserSerializer, AuthTokenSerializer
+from core.models import User
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -17,11 +20,12 @@ class CreateTokenView(ObtainAuthToken):
     # Responsible for the ppost button(view)
 
 
-class ManageUserView(generics.RetrieveUpdateAPIView):
+class ManageUserView(generics.RetrieveUpdateDestroyAPIView):
     """Manage the authenticated user"""
     serializer_class = UserSerializer
     authentication_classes = [authentication.TokenAuthentication, ]
     permission_classes = [permissions.IsAuthenticated, ]
+    queryset = User.objects.all()
 
     def get_object(self):
         """Retrieve and return authentication user"""
